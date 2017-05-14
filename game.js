e38b1a140cb6c185d235d8a39c5642f70bf91837
nreddy216 <nreddy216@gmail.com>
2017-05-14 16:35:03 -0700

    beginning work on component building

--- game.js
+++ game.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Algebra from './src/algebra';
-import './dist/styles.css'
 
 // TODO: Create elements by random
 // Have random number generator for x + (random number from -10 to 10) = (random number from -10 to 10)
 ReactDOM.render(
-  // React.createElement(Algebra, {});
-  ///
-  <div>Hello World</div>
+  <Algebra />
 );
--- src/algebra.js
+++ src/algebra.js
@@ -1,3 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 // Helps conditionally join class names together
 import classNames from 'classnames';
+
+// TODO: Move this to helper file or X component
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let x = getRandomInt(-10, 10);
+
+console.log(x);
+
+// Components
+
+// Box - 'dumb' component, only renders out box
+class Box extends Component {
+  render() {
+    return (
+      <div className="box">
+        <p>1</p>
+      </div>
+    )
+  }
+}
+
+// Main component
+class Algebra extends Component {
+  init() {
+    console.log('init');
+  }
+
+  render() {
+    <div className="Algebra">
+      <Box />
+    </div>
+  }
+}

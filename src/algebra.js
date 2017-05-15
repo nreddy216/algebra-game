fec5b517fb8a0af82d30b3d06c8340a60111a1cc
Nidhi Reddy <Nidhi.Reddy@akqa.com>
2017-05-15 11:22:54 -0700

    add state for boxes for weights and rendering initial num of boxes, update styling to be more like balance

--- src/algebra.js
+++ src/algebra.js
@@ -11,59 +11,88 @@ function getRandomInt(min, max) {
 // Components
 // Main component
 class Algebra extends Component {
-  constructor(props) {
-    super(props);
-    console.log('init');
-    // this.props = {
-    //   numBoxes: 5
-    // }
+  constructor() {
+    super();
+
+    const xValue = getRandomInt(-5, 5);
+    const rightValue = xValue + getRandomInt(-5, 5);
     this.state = {
       angle: 180,
-      numBoxes: 5
+      xValue: xValue,
+      numBoxesRight: xValue + rightValue,
+      numBoxesLeft: rightValue - xValue
     }
+  }
+
+  addBox(){
+    // this.setState({
+    //   numBoxes: this.state.numBoxes + 1
+    // })
   }
 
   render() {
     return (
       <div className="Algebra">
-        <Box state={this.state} {...this.props} />
-        <MysteryBox {...this.props} />
-        <Balance state={this.state} {...this.props} />
+        <div className="Boxes">
+          <BoxContainer numBoxes={Math.abs(this.state.numBoxesLeft)} />
+          <BoxContainer numBoxes={Math.abs(this.state.numBoxesRight)} />
+        </div>
+        <MysteryBox addBox={this.addBox.bind(this)} xValue={this.state.xValue} />
+        <Balance />
       </div>
     );
   }
 }
 
 // Mystery Box
-const MysteryBox = () => {
-  return (
-    <div className="MysteryBox" value={getRandomInt(-10, 10)}>
-    </div>
-  );
+class MysteryBox extends Component {
+
+  render() {
+    return (
+      <div className="MysteryBox" onClick={this.props.addBox} value={this.props.xValue}>
+      </div>
+    );
+  }
 };
 
-// Box - 'dumb' component, only renders out box
+class BoxContainer extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    let componentArray = [];
+
+    for(var i=0; i<this.props.numBoxes; i++){
+      componentArray.push( <Box key={i} />);
+    }
+
+    return (
+      <div className="BoxContainer">
+        {componentArray}
+      </div>
+    )
+  }
+
+}
+
+// Box
 class Box extends Component {
 
   constructor(props){
     super(props);
-    // this.props.numBoxes
-
   }
 
   render(){
-    let componentArray = [];
-    for(var i=0; i<=this.props.numBoxes; i++) {
-      componentArray.push(<div className="Box" key={i} value='1'></div>);
-    }
     return (
-      <div>
-        {componentArray}
+      <div className="Box" value="1">
       </div>
     );
   }
 }
 
+// Balance
+// state : rotation of balance shifts depending on # boxes on each side of balance
 class Balance extends Component {
   constructor(props){
     super(props);
@@ -72,9 +101,23 @@ class Balance extends Component {
   render(){
     return (
       <div className="Balance" value='180'>
+        <div className="pivot">
+        </div>
       </div>
     );
   }
 };
+
+// class LeftSide extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render(){
+//     return (
+//
+//     )
+//   }
+// }
 
 export default Algebra;
